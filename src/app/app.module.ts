6a24e695ff07222101d99dba643c312810611eb8
David <dav.baz@alice.it>
2022-04-04 00:49:54 +0200

    firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,FormsModule
+    BrowserModule,FormsModule,
+     provideFirebaseApp(() => initializeApp(environment.firebase)),
+      provideDatabase(() => getDatabase()), provideFirestore(() => getFirestore()),
+      
   ],
   providers: [],
   bootstrap: [AppComponent]
