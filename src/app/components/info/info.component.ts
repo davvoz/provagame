cb4f5ff62172e7bc8bad32630e668e11e5c4f15f
David <dav.baz@alice.it>
2022-04-19 21:14:06 +0200

    last sonar

--- src/app/components/info/info.component.ts
+++ src/app/components/info/info.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 import { collectionData } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { IMyGeolocation, IMyUint8Array } from 'src/app/classes/utils/costants.enum';
-import { Utilities } from 'src/app/classes/utils/utilities';
 import { FirebaseService } from 'src/app/services/firebase.service';
 
 @Component({
@@ -29,16 +28,8 @@ export class InfoComponent {
     this.http.get('https://geolocation-db.com/json/').subscribe(
       (reso: any) => {
         this.out = reso;
-        // if (!this.added) {
-        //   this.fservice.addItem({ dato: reso, tabella: 'location' });
-        //   this.added = true;
-        //   const ba: IMyUint8Array = {
-        //     byteArrayStringFormat: Utilities.bin2String(["0101", "0101", "0100", "0101", "0011", "1001", "0101", "0011", "0101", "0001", "0011", "0000", "0100", "0101", "0110", "0111", "0101", "0110", "0100", "0110", "0100", "1010", "0101", "0000", "0101", "0011", "0101", "0101", "0100", "0101", "0110", "0111", "0101", "0001", "0011", "0000", "0110", "1000", "0100", "0110", "0100", "1001", "0100", "0101", "0100", "1010", "0100", "0110", "0101", "0100", "0100", "0101", "0111", "1000", "0101", "0000", "0100", "1001", "0100", "0101", "0110", "1100", "0100", "1101", "0100", "1001", "0100", "0101", "0100", "1110", "0100", "0010", "0101", "0111", "0110", "1100", "0111", "0000", "0101", "0000"])
-        //   }
-        //   console.log(ba);
-        //   this.fservice.addItem({ dato: ba, tabella: 'byteArray' });
-        //}
       });
+
     // @ts-ignore
     this.item$ = collectionData(this.fservice.getLista('byteArray'));
   }
